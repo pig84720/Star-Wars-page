74304013da717e4abfba5c7116b2012a3ef74a00
YuTingDong <pig8472000@gmail.com>
2020-02-18 12:29:49 +0800

    重新排版

--- main.js
+++ main.js
@@ -1,21 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    var screen_height = document.body.clientHeight;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > screen_height) {
+            $(".navbg").addClass("navbar-fixed");
+            $(".about-us").addClass("about-us_area");
+        } else if ($(window).scrollTop() < screen_height + 1) {
+            $(".navbg").removeClass("navbar-fixed");
+            $(".about-us").removeClass("about-us_area");
+        }
+    });
 
-/*map*/
+    /*點擊超連結滑動*/
+    $("#logo").click(function() {
+        $("html, body").animate({ scrollTop: $("#home").offset().top }, 500);
+    });
 
-var map = new GMaps({
-    div: '.map',
-    lat: 25.085914,
-    lng: 121.5191553
-  });
+    $("#btn").click(function() {
+        $("html, body").animate({ scrollTop: $("#about-us").offset().top }, 500);
+    });
 
-map.addMarker({
-    lat: 25.085914,
-    lng: 121.5191553,
-    title: '絕地聖殿',
-    infoWindow: {
-        content: '<p>絕地聖殿</p>'
-      }
-});
+    $("#nav-about").click(function() {
+        $("html, body").animate({ scrollTop: $("#about-me").offset().top }, 500);
+    });
 
+    $("#process").click(function() {
+        $("html, body").animate({ scrollTop: $("#relation").offset().top }, 500);
+    });
 
+    $("#pic").click(function() {
+        $("html, body").animate({ scrollTop: $("#photo").offset().top }, 500);
+    });
+
+    $("#contact-us").click(function() {
+        $("html, body").animate({ scrollTop: $("#contact").offset().top }, 500);
+    });
+
+    $("#position").click(function() {
+        $("html, body").animate({ scrollTop: $("#contact").offset().top }, 500);
+    });
+    /*map*/
+
+    var map = new GMaps({
+        div: ".map",
+        lat: 25.085914,
+        lng: 121.5191553
+    });
+
+    map.addMarker({
+        lat: 25.085914,
+        lng: 121.5191553,
+        title: "絕地聖殿",
+        infoWindow: {
+            content: "<p>絕地聖殿</p>"
+        }
+    });
 });
